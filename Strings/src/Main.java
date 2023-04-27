6f58788f4079efdb589e0df55c754b405a6bcea1
RISHI6644\RISHI <hrishikeshdas111@gmail.com>
2023-04-28 00:49:42 +0600

    Practiced String pool and new keyword

--- Strings/src/Main.java
+++ Strings/src/Main.java
@@ -5,5 +5,26 @@ public class Main {
         System.out.println(str.length());
         System.out.println(str.charAt(1));
         System.out.println("Let's Start!".length());
+
+        String s1 = "abc";
+        String s2 = "abc";
+
+        if (s1 == s2) {
+            System.out.println("Equal");
+        }
+        else {
+            System.out.println("Not Equal");
+        }
+
+        String s3 = new String("abc");
+
+        // if (s1 == s3)
+        if (s1.equals(s3))
+        {
+            System.out.println("Equal");
+        }
+        else {
+            System.out.println("Not Equal");
+        }
     }
 }
