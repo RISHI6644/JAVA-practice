1f322158db89fc96b1f3b0c0568b2b1aa20375eb
RISHI6644\RISHI <hrishikeshdas111@gmail.com>
2023-04-27 11:50:33 +0600

    practiced string classes object creation and used length () & charAt() method

--- Strings/src/Main.java
+++ Strings/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Let's learn Strings!");
+        String str = "Let's learn Strings!";
+        System.out.println(str);
+        System.out.println(str.length());
+        System.out.println(str.charAt(1));
+        System.out.println("Let's Start!".length());
     }
 }
