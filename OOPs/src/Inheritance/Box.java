8f528c56f48ed0b51f515f682b7d4ca01a2265d7
RISHI6644\RISHI <hrishikeshdas111@gmail.com>
2023-05-23 21:58:36 +0600

    Created Inheritance package

---
+++ OOPs/src/Inheritance/Box.java
@@ -0,0 +1,7 @@
+package Inheritance;
+
+public class Box {
+    double length;
+    double height;
+    double weight;
+}
---
+++ OOPs/src/Inheritance/Main.java
@@ -0,0 +1,21 @@
+package Inheritance;
+
+/* Inheritance: Consider the following scenario:
+        In real world, parents pass their properties such as money, traits, values etc. to their children. So, children
+        can inherit the properties from their parents. And that is known as inheritance.
+
+        In terms of classes, let's say if there is a class that can use the properties of another class will be called
+        as base class or parent class. And the class which inherits the properties from base class will be called as
+        child class.
+
+        To inherit the base class defines to incorporate the definition of a class to another class(child). And to do
+        that we can use 'extends' keyword.
+
+        Syntax: class ChildClass extends BaseClass {}
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Let's learn about Inheritance!!");
+    }
+}
