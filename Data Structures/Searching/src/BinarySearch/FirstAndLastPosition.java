14108014993ffbada402c2bfe96e6228bea39a16
RISHI6644\RISHI <hrishikeshdas111@gmail.com>
2023-08-13 22:19:34 +0600

    Solved the "First and last position of an element" probelm by using BS

---
+++ Data Structures/Searching/src/BinarySearch/FirstAndLastPosition.java
@@ -0,0 +1,58 @@
+package BinarySearch;
+
+// Question: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+// Find first occurrence of the target
+// Find last occurrence of the target
+
+import java.util.Arrays;
+
+public class FirstAndLastPosition {
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8;
+        int[] out = positionSearch(nums, target);
+
+        System.out.println(Arrays.toString(out));
+    }
+
+    public static int[] positionSearch(int[] arr, int target) {
+        int[] position = {-1, -1};
+
+        // Check for first occurrence or, if the target is first
+        int start = searchIndex(arr, target, true);
+        int end = searchIndex(arr, target, false);
+
+        position[0] = start;
+        position[1] = end;
+
+        return position;
+    }
+
+    // This function just returns the index value of target
+    public static int searchIndex(int[] arr, int target, boolean findStartIndex) {
+        int index = -1;
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int middle = (start + end) / 2;
+
+            if (target < arr[middle]) {
+                end = middle - 1;
+            }
+            else if (target > arr[middle]){
+                start = middle + 1;
+            }
+            else {
+                index = middle; // potential index found
+                if (findStartIndex) {
+                    end = middle - 1;
+                }
+                else {
+                    start = middle + 1;
+                }
+            }
+        }
+        return index;
+    }
+}
